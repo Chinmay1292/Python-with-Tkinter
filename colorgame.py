42558b1890470e89a028cf656f095be51db5808a
Chinmay1292 <chinmaychougule1212@gmail.com>
2022-02-22 22:43:20 +0530

    color game

--- colorgame.py
+++ colorgame.py
@@ -3,3 +3,7 @@ import random
 colours = ['Red','Blue','Green','Pink','Black', 'Yellow','Orange','White','Purple','Brown'] 
 score = 0
 timeleft = 30 
+def startGame(event):
+    if timeleft == 30:
+        countdown()
+    nextColour() 
