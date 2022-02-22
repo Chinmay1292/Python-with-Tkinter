254b3830832ae822ec9835f0b559ed39c0fb9b26
Chinmay1292 <chinmaychougule1212@gmail.com>
2022-02-22 22:46:33 +0530

    color game

--- colorgame.py
+++ colorgame.py
@@ -7,3 +7,14 @@ def startGame(event):
     if timeleft == 30:
         countdown()
     nextColour() 
+def nextColour():
+    global score 
+    global timeleft
+    if timeleft > 0:
+        e.focus_set()
+        if e.get().lower() == colours[1].lower():
+            score += 1
+        e.delete(0, tkinter.END)
+        random.shuffle(colours)
+        label.config(fg = str(colours[1]), text = str(colours[0]))
+        scoreLabel.config(text = "Score: " + str(score))
