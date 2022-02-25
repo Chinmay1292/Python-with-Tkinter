d67545ca20349a45e1f3176a1bb26075acdf463c
Chinmay1292 <chinmaychougule1212@gmail.com>
2022-02-25 23:34:44 +0530

    currency converter

--- currencyconverter.py
+++ currencyconverter.py
@@ -9,3 +9,13 @@ root.geometry("600x400")
 
 Tops = Frame(root,pady = 2, width =3000, height = 100, relief = "ridge")
 Tops.grid(row=0,column=0)
+
+
+headlabel = tk.Label(Tops,font=('lato black', 19,'bold'), text = '      TechnoLord Programming : Currency Converter     ') 
+headlabel.pack(padx=10, pady=0)
+ 
+variable1 = tk.StringVar(root) 
+variable2 = tk.StringVar(root) 
+ 
+variable1.set("currency") 
+variable2.set("currency") 
