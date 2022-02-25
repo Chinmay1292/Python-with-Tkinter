70171f3db3d42326136be050c75af020f533496b
Chinmay1292 <chinmaychougule1212@gmail.com>
2022-02-26 00:20:56 +0530

    currency converter

--- currencyconverter.py
+++ currencyconverter.py
@@ -19,3 +19,21 @@ variable2 = tk.StringVar(root)
  
 variable1.set("currency") 
 variable2.set("currency") 
+
+def RealTimeCurrencyConversion(): 
+    from forex_python.converter import CurrencyRates
+    c=CurrencyRates()
+    
+    from_currency = variable1.get() 
+    to_currency = variable2.get()
+    
+    if (Amount1_field.get()==""):
+        tkinter.messagebox.showinfo("Error !!","Amount Not Entered.\n Please a valid amount.")
+        
+    elif (from_currency=="currency" or to_currency=="currency"):
+        tkinter.messagebox.showinfo("Error !!","Currency Not Selected.\n Please select FROM and TO Currency form menu.")
+        
+    else:
+        new_amt = c.convert(from_currency,to_currency,float(Amount1_field.get()))
+        new_amount = float("{:.4f}".format(new_amt))
+        Amount2_field.insert(0, str(new_amount)) 
