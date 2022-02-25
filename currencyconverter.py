071762fe8556237f1230252c2cc2c08e03f7010f
Chinmay1292 <chinmaychougule1212@gmail.com>
2022-02-25 23:30:54 +0530

    currency

---
+++ currencyconverter.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import *
+import tkinter.messagebox
+ 
+root = tk.Tk() 
+
+root.title("GUI : Currency Conversion")
+root.geometry("600x400")
+
+Tops = Frame(root,pady = 2, width =3000, height = 100, relief = "ridge")
+Tops.grid(row=0,column=0)
